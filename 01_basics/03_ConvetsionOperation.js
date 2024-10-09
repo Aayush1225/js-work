657eeda99afc9d954e2ee869ab8e37b85370f8bb
Aayush1225 <141708740+Aayush1225@users.noreply.github.com>
2024-10-09 13:40:21 +0000

    Conversion of datatypes

---
+++ 01_basics/03_ConvetsionOperation.js
@@ -0,0 +1,28 @@
+let score = "aayush"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+/*
+"33" => 33
+"33def" => NaN  not an number
+true => 1
+*/
+
+let isLoggedIn = "aayush"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 =>true ; 0=> false
+// "" => false
+//"aayush" => true
+
+let someNumber = 99
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
