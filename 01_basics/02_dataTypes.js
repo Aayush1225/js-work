074854392e3c8723d49f3a84e6c974611dd7d157
Aayush1225 <141708740+Aayush1225@users.noreply.github.com>
2024-10-09 11:54:20 +0000

    learning a datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";     // treat all JS code as newer version
+
+//alert(4+5)   // we are use in node js , not in browser
+
+let name = "aayush "
+let age = 17
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigint 
+//string => ""
+//boolean => true/falsee
+//null => standalone value 
+// undefined =>
+    // symbol  => unique
+
+
+// object
+
+console.log(typeof "aayush");
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
