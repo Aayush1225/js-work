053c52c372361811b7ba8e96d52adde778057f7c
Aayush1225 <141708740+Aayush1225@users.noreply.github.com>
2024-10-24 11:20:13 +0000

    number and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,43 @@
+const score = 400
+console.log(score);
+
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
+
+const otherNumber = 123.8485
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreds =10000000000
+console.log(hundreds.toLocaleString('en-In'));
+
+//****************************  Maths ***************************** */
+
+console.log(Math);
+
+console.log(Math.abs(-7))
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,5,3,7,1,9));
+console.log(Math.min(4,5,3,7,1,9));
+console.log(Math.max(4,5,3,7,1,9));
+
+console.log((Math.random()*20)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) ) + min)
+
+
+
+
+
+
+
+
