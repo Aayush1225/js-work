fe2e9912f9a14c1448d5aecc17c150d745e0000b
Aayush1225 <141708740+Aayush1225@users.noreply.github.com>
2024-10-26 16:08:22 +0000

    Arrays first vedio

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,52 @@
+/*************************   Arrays   ******************** */
+
+const myArr = [0, 1, 2, 3 , 4, 5, 6]
+const pubgPlayers =["Nofear","Mafianinja",'Sky']
+//const myArr2 = new myArr2("ayush","acharya", 1, 2 , 3)
+
+//console.log(myArr[3])
+
+
+/*******   Arrays Methods ************ */
+
+//myArr.push(7)
+//myArr.pop(7)
+
+//myArr.unshift(9)
+//myArr.shift()
+
+//console.log(myArr.includes(9));
+//console.log(myArr.indexOf(6));
+
+//const newArr = myArr.join()
+
+
+//console.log(myArr);
+//console.log(newArr);
+
+//console.log(typeof newArr);
+
+
+
+//   Slice  , splice  
+
+console.log("A ",myArr);
+
+const myN1 = myArr.slice(1, 3)
+console.log(myN1);
+
+console.log("B",myArr);
+
+const myN2 = myArr.splice(1 ,3)
+console.log("C",myArr);
+
+console.log(myN2);
+
+
+
+
+
+
+
+
+
