1821ce44663e371362843c7834bca1ffd9d937f3
Aayush1225 <141708740+Aayush1225@users.noreply.github.com>
2024-10-27 07:33:32 +0000

    array method

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,37 @@
+const fruits =['Apple','Mango','Banana']
+const vegetables =['Potato','Cabbage','Tomato']
+
+//fruits.push(vegetables)
+//console.log(fruits);
+
+ //const finalResult =  fruits.concat(vegetables)
+//console.log(finalResult);
+
+//const finalResult = [...fruits,...vegetables]
+//console.log(finalResult);
+
+
+  const arr2 = [1,2,3,4 ,[5,6,7],8,9,[10,11,[12,13,[14,15]]]]
+  const real_arr2 =arr2.flat(Infinity)
+  console.log(real_arr2);
+
+
+
+ console.log( Array.isArray('Ayush'))
+ console.log( Array.from('Ayush'))
+ console.log( Array.from({name: "ayush"}))   // intresting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
+
+
+
+  
+
+
+
+
+
