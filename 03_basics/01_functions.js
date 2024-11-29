ff286f85d8d0211c786b56cc503488df1404abbf
Aayush1225 <141708740+Aayush1225@users.noreply.github.com>
2024-11-29 13:58:45 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -23,7 +23,7 @@ function addTwoNumber( number1,number2 ){
     
 }
 const result = addTwoNumber(4,3)
-console.log( result);
+//console.log( result);
 
 //console.log("Results: " result );
 
@@ -51,7 +51,40 @@ function loginUsermessage(username){
 }
 
 //console.log(loginUsermessage("Ayush"))
-console.log(loginUsermessage("ayush"))
+//console.log(loginUsermessage("ayush"))
+
+
+function calculateCartPrize(val1, val2, val3 ,...num1){
+    return num1  
+    
+}
+//console.log(calculateCartPrize(200,300,400,500,600))
+
+
+let user = {
+    username:"Ayush",
+    salary:3000
+
+}
+
+function userHandle (anyObject){
+    console.log(`user Name is ${anyObject.username} and salary is ${anyObject.salary}`);
+    
+}
+//userHandle(user)
+userHandle({
+    username:"Puzan",
+    salary:10000
+})
+
+ const myNewarray =[200,300,400,]
+
+ function returnSecondValues(anyarrays){
+    return anyarrays[2]
+ }
+//  console.log(returnSecondValues(myNewarray));
+console.log(returnSecondValues([400,500,600]));
+ 
 
 
 
