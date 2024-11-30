9134abc4b273f13644e3c4534a8357f048a7f24e
Aayush1225 <141708740+Aayush1225@users.noreply.github.com>
2024-11-30 12:04:05 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,75 @@
+let num1 = 10
+// const num2 =20
+// var num3 = 30
+
+if(true){
+    let num1 = 100
+    // const num2 = 200
+    // var num3 = 300
+
+
+
+    // console.log("Inner :" + num1);
+    
+}
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+
+
+
+function one(){
+    const username = "Ayush"
+
+    function two(){
+        const age = 17
+        // console.log(username)
+    console.log(age);
+
+    }
+    two()
+    console.log(username)
+    
+}
+// one()
+
+
+
+
+if (true) {
+    const username = "Haseni"
+    
+    if (username === "Haseni") {
+        const website = "facebook"
+     //console.log(website);
+
+    }
+    
+        // console.log(username);
+    
+}
+
+
+
+
+
+// ++++++++++++++++++     Intersting      ++++++++++++++++++++++++++++++
+
+
+console.log(addone(7))
+function addone (num){
+    return num+1
+}
+
+
+
+//console.log(holdFunction(6))
+const holdFunction = function addtwo(num){
+    return num+2
+}
+ console.log(holdFunction(6))
+
+
+
