93b4c54dc4eb04fa25e4cd12688db4b8d2433e9e
Aayush1225 <141708740+Aayush1225@users.noreply.github.com>
2024-12-01 16:53:08 +0000

    this and arrows

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,63 @@
+const user = {
+    username : 'Ayush',
+    prize : 2000,
+
+    welcomeMessage : function(){
+        console.log(`${this.username } , welcome to website`);
+       // console.log(this);
+        
+    }
+}
+// user.welcomeMessage()
+// user.username = 'Haseni'
+// user.welcomeMessage()
+
+ //console.log(this);
+
+
+
+//  function one (){
+//     username : "Ayush"
+//     console.log(this.username);
+    
+//  }
+//  one()
+
+
+
+// const chai = function (){
+//     username : "Ayush"
+//   console.log(this.username);
+// }
+
+// chai()
+
+
+
+const chai =  ()  =>{
+    username : "Ayush"
+  console.log(this.username);
+}
+
+// chai()
+
+
+
+ 
+// const addTwo = (num1,num2 ) =>{
+//     return num1 + num2
+//  }
+//  console.log(addTwo(4,5));
+
+
+  
+// const addTwo = (num1,num2 ) =>    num1 + num2
+const addTwo = (num1,num2 ) =>    (num1 + num2)
+
+ 
+ console.log(addTwo(4,5))
+
+
+ 
+
+
