5cd093d869b415db456e03fd91894f523a6992eb
Aayush1225 <141708740+Aayush1225@users.noreply.github.com>
2025-01-07 06:19:22 +0000

    IIFE concept

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,10 @@
+// Immediately Invoked Function Experssion (IIFE)
+
+(function one (){
+    console.log(`My name is Ayush`);
+    
+})();
+
+((place)=>{
+    console.log(`I lived in ${place}`);
+})('Pokhara')
