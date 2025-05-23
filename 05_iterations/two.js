0cce9b46a29beeba50c9624f384778b926d856f6
Aayush1225 <141708740+Aayush1225@users.noreply.github.com>
2025-05-23 06:51:49 +0000

    while and do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,29 @@
+//    *****************  while lop   ***********
+
+let index = 0
+while (index <= 10) {
+  //  console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+
+
+let myArray = [ "crickwt", "football", "baseball"]
+let arr = 0
+
+while (arr< myArray.length) {
+    // console.log(`Vallue is ${myArray[arr]}`);
+    arr = arr + 1
+    
+}
+
+
+
+// ****************  D0 While   ***************
+
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score ++
+} while (score <= 10);
